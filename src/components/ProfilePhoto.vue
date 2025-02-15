46e34e55774ac45268a6bfff492ab0b36938fa7c
Wizxrdx <benedick.dumol@gmail.com>
2025-02-15 11:29:52 +0800

    created: ProfilePhoto.vue component

---
+++ src/components/ProfilePhoto.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="profile-container">
+    <div class="profile-photo">
+      <img src="https://avatars.githubusercontent.com/u/50022009?v=4" alt="Profile Photo" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Centering the profile picture */
+.profile-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 5vw;
+  flex: 7 7 auto;
+}
+
+/* Profile Picture */
+.profile-photo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  overflow: hidden;
+  position: relative;
+  box-shadow: 0 0 50px rgba(255, 255, 255, 0.2);
+  transition: box-shadow 1s ease-in-out;
+}
+
+.profile-photo img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  filter: brightness(1);
+  transition: filter 0.5s ease-in-out;
+}
+
+.profile-photo:hover {
+  box-shadow: 0 0 80px rgba(255, 255, 255, 0.1);
+}
+
+.profile-photo:hover img {
+  filter: brightness(1.2);
+}
+</style>
