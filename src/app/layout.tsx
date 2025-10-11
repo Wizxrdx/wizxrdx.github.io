1e8fe4047de2449c45d2764441953be5ad107569
Wizxrdx <benedick.dumol@gmail.com>
2025-10-11 21:01:07 +0800

    Refactor layout and styles: integrate Google Fonts, adjust font-family, and enhance content layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,27 @@ import Background from "../components/Background/Background";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
+import { IBM_Plex_Sans, Newsreader } from 'next/font/google'
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex-sans',
+})
+
+const newsreader = Newsreader({
+      weight: ['400', '700'],
+      subsets: ['latin'],
+      variable: '--font-newsreader',
+      display: 'swap',
+    });
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${ibmPlexSans.variable} ${newsreader.variable}`}>
       <body>
         <Background />
         <div className="layout">
