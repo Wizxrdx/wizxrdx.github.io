84de05591b0d2cf10b51b96424a94d29ec64ba97
Wizxrdx <benedick.dumol@gmail.com>
2025-10-12 06:37:18 +0800

    Refactor AboutPage: enhance profile section with contact links and update styling for better presentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import TextWritingAnimation from '@/components/TextTypingAnimation/TextTypingAnimation';
 import styles from './page.module.css'
+import Link from 'next/link';
 
 export default function AboutPage() {
   return (
@@ -13,10 +14,31 @@ export default function AboutPage() {
       </div>
 
       <article className={styles.section}>
+        <div className={styles.profile}>
         <img className={styles.profileImage} src="/profile.png" alt="Wizxrdx" />
+        <div className={`${styles.profileContents} ${'glass-card'}`}>
+          <Link href='mailto:benedick.dumol@gmail.com'>
+            benedick.dumol@gmail.com
+          </Link>
+          <Link href='https://github.com/wizxrdx' target='_blank' rel='noopener noreferrer'>
+            GitHub
+          </Link>
+          <Link href='https://www.linkedin.com/in/benedick-labbao/' target='_blank' rel='noopener noreferrer'>
+            LinkedIn
+          </Link>
+        </div>
+        </div>
         <p className={styles.sectionBody}>
-          Entry-level Machine Learning and Full-Stack Developer with hands-on experience in
-          AI systems, data processing, and end-to-end application development.
+          Hello! I'm <b>Benedick Labbao</b>, an aspiring <b>Machine Learning Engineer</b> and <b>Full-Stack Developer</b>.
+          I am a <b>Computer Engineering</b> undergraduate in <b>Technological Institute of the Philippines - Quezon City</b>,
+          Specializing in <b>Data Science</b>. I focus on <b>AI systems</b>, <b>data processing</b>, and <b>end-to-end application development</b>.
+        </p>
+        <p className={styles.sectionBody}>
+          My expertise includes <b>Machine Learning</b>, <b>Deep Learning</b>, <b>Data Analysis</b>, and <b>Full-Stack Development</b>.
+          I build AI models, analyze data, and create web applications.
+        </p>
+        <p className={styles.sectionBody}>
+          My hobbies right now include IoT projects, gaming, and exploring new technologies.
         </p>
       </article>
     </div>
