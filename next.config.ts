5fae608e199d822b6bb0830dee231df40cb21e7c
Wizxrdx <benedick.dumol@gmail.com>
2025-10-11 13:40:11 +0800

    Update next.config.ts to set output option for static file export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Ensures the site is exported as static files
 };
 
 export default nextConfig;
