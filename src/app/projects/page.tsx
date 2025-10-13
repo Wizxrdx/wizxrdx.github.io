7bd108a2d52afe80416c3b76298f37405d24cc7c
Wizxrdx <benedick.dumol@gmail.com>
2025-10-13 11:25:53 +0800

    Refactor ProjectsPage component: update header styles, adjust font sizes, and remove commented-out code for cleaner presentation

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,84 +11,10 @@ export default function ProjectsPage() {
             </div>
             <div className={styles.itemContainer}>
 
-                {/* <div className={`${styles.item} ${'glass-card'}`}>
-                    <img className={styles.itemImagePreview} src="https://raw.githubusercontent.com/Wizxrdx/nasa-space-apps-2025/refs/heads/main/assets/home_page_1.png" alt="test" />
-                    <div className={styles.itemContent}>
-                        <h2 className={styles.itemTitle} >GitHub Portfolio Website</h2>
-                        <p className={styles.itemDesc}>A personal website showcasing my projects and skills.</p>
-                        <div className={styles.itemTags}>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                        </div>
-                        <div className={styles.itemFeatures}>
-                            <h4>Features:</h4>
-                        <ul>
-                            <li>Personal Website</li>
-                            <li>Open Source</li>
-                            <li>Web Development</li>
-                            <li className={styles.itemHeaderDate}>January 2024 - Present</li>
-                        </ul>
-                        </div>
-                        <div className={styles.itemButton}>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.code}>Code</Link>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.youtube}>Demo</Link>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.visit}>Visit</Link>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.paper}>Paper</Link>
-                        </div>
-                    </div>
-                </div> */}
-
-                {/* <div className={`${styles.item} ${'glass-card'}`}>
-                    <iframe className={styles.itemVideoPreview} src="https://www.youtube.com/embed/" title='Video Demo'></iframe>
-                    <div className={styles.itemContent}>
-                        <h2 className={styles.itemTitle} >GitHub Portfolio Website</h2>
-                        <p className={styles.itemDesc}>A personal website showcasing my projects and skills.</p>
-                        <div className={styles.itemTags}>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                            <span>Next.js</span>
-                            <span>TypeScript</span>
-                            <span>CSS</span>
-                        </div>
-                        <div className={styles.itemFeatures}>
-                            <h4>Features:</h4>
-                        <ul>
-                            <li>Personal Website</li>
-                            <li>Open Source</li>
-                            <li>Web Development</li>
-                            <li className={styles.itemHeaderDate}>January 2024 - Present</li>
-                        </ul>
-                        </div>
-                        <div className={styles.itemButton}>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.code}>Code</Link>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.youtube}>Demo</Link>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.visit}>Visit</Link>
-                            <Link href="/projects/hybrid-exoplanet-recognition" target="_blank" rel="noopener noreferrer" className={styles.paper}>Paper</Link>
-                        </div>
-                    </div>
-                </div> */}
-
                 <div className={`${styles.item} ${'glass-card'}`}>
                     <iframe className={styles.itemVideoPreview} src="https://www.youtube.com/embed/5agHeSbKPbs" title='Hybrid Exoplanet Recognition and Model Evaluation System'></iframe>
                     <div className={styles.itemContent}>
-                        <h2 className={styles.itemTitle} >Hybrid Exoplanet Recognition and Model Evaluation System - HERMES</h2>
+                        <h3 className={styles.itemTitle} >Hybrid Exoplanet Recognition and Model Evaluation System - HERMES</h3>
                         <p className={styles.itemDesc}>HERMES is a web application designed for the <strong>NASA Space Apps Challenge 2025</strong>, helping users classify exoplanet candidates efficiently. It allows users to upload or edit exoplanet datasets and run a machine learning model to identify confirmed planets, potential candidates, or false positives.</p>
                         <div className={styles.itemTags}>
                             <span>Web Application</span>
@@ -122,7 +48,7 @@ export default function ProjectsPage() {
                 <div className={`${styles.item} ${'glass-card'}`}>
                     <iframe className={styles.itemVideoPreview} src="https://www.youtube.com/embed/ZG2r1KPy5RM" title='AI-Powered Mosquito Monitoring System'></iframe>
                     <div className={styles.itemContent}>
-                        <h2 className={styles.itemTitle} >AI-Powered Mosquito Monitoring System</h2>
+                        <h3 className={styles.itemTitle} >AI-Powered Mosquito Monitoring System</h3>
                         <p className={styles.itemDesc}>Research and prototype of an attractant-based trap with image recognition to identify Aedes mosquitoes, with a companion mobile app for real-time location monitoring and notifications. The system&apos;s detection accuracy was evaluated under engineering performance and efficiency constraints.</p>
                         <div className={styles.itemTags}>
                             <span>Internet of Things</span>
@@ -155,7 +81,7 @@ export default function ProjectsPage() {
                 <div className={`${styles.item} ${'glass-card'}`}>
                     <iframe className={styles.itemVideoPreview} src="https://www.youtube.com/embed/SsIo-v1et94" title='Smart Packaging Control & Demand Forecasting Web Application'></iframe>
                     <div className={styles.itemContent}>
-                        <h2 className={styles.itemTitle} >Smart Packaging Control & Demand Forecasting Web Application</h2>
+                        <h3 className={styles.itemTitle} >Smart Packaging Control & Demand Forecasting Web Application</h3>
                         <p className={styles.itemDesc}>Developed a web application deployed on a Raspberry Pi to remotely monitor and control a packaging machine. Integrated a SARIMA forecasting model into the app to predict daily packaging demand, achieving a 4.61% MAPE. The project scope focused solely on the software and system integration.</p>
                         <div className={styles.itemTags}>
                             <span>Industrial Automation</span>
@@ -188,7 +114,7 @@ export default function ProjectsPage() {
                 <div className={`${styles.item} ${'glass-card'}`}>
                     <img className={styles.itemImagePreview} src="/image1.png" alt="Exercise Posture Suggestion System" />
                     <div className={styles.itemContent}>
-                        <h2 className={styles.itemTitle} >Exercise Posture Suggestion System</h2>
+                        <h3 className={styles.itemTitle} >Exercise Posture Suggestion System</h3>
                         <p className={styles.itemDesc}>An Exercise Posture Suggestion System using deep learning to analyze and provide real-time feedback on correct exercise posture. Integrated pose estimation, human activity recognition, and error detection algorithms to offer personalized guidance. The software interface, built in Unity, allows users to visualize correct postures and optimize workouts, aiming to reduce injury risk and improve exercise effectiveness.</p>
                         <div className={styles.itemTags}>
                             <span>Real-Time Feedback Systems</span>
